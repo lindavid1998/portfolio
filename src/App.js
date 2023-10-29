036190aa2aeea3d0cc7a69dd1d4830824a9fc081
David Lin <lindavid1998@gmail.com>
2023-10-29 15:25:19 -0700

    Add Footer

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import React from 'react'
 import Navbar from './Navbar'
 import Hero from './Hero';
 import styled from 'styled-components';
+import Projects from './Projects';
+import Footer from './Footer';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 40px;
+  padding: 30px;
 `
 
 const App = () => {
-  return <Container>
-    <Navbar />
-    <Hero />
-  </Container>;
+  return (
+		<Container>
+			<Navbar />
+			<Hero />
+      <Projects />
+      <Footer />
+		</Container>
+	);
 }
 
 export default App
