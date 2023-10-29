d566faedc121d5ae60b6fb5695ef64e17234121f
David Lin <lindavid1998@gmail.com>
2023-10-29 13:26:18 -0700

    Implement Button component

---
+++ src/Button.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const colorStyles = {
+	primary: {
+		color: 'var(--background-color)',
+		backgroundColor: 'var(--accent-color)',
+	},
+	secondary: {
+		color: 'var(--accent-color)',
+		backgroundColor: 'var(--background-color)',
+	},
+};
+
+const StyledButton = styled.button`
+	padding: 10px;
+	font-size: 1.3rem;
+	border-radius: 5px;
+	border: 1px solid var(--accent-color);
+	font-weight: bold;
+	cursor: pointer;
+	color: ${(props) => colorStyles[props.color].color};
+	background-color: ${(props) => colorStyles[props.color].backgroundColor};
+	transition: transform 0.3s ease-in-out;
+	&:hover {
+		transform: translate(0, -5px);
+	}
+`;
+
+const Button = ({ color, text }) => {
+	return <StyledButton color={color}>{text}</StyledButton>;
+};
+
+export default Button;
