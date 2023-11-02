be945dfacc43a0c5bb2df01dcbc4bb818759532a
David Lin <lindavid1998@gmail.com>
2023-11-01 18:02:00 -0700

    Add resume download to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as DownloadIcon } from '../assets/download-solid.svg';
+import ResumePdf from '../assets/Resume_Nov2023.pdf'
 
 const Nav = styled.nav`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  padding: 10px 0;
-  font-weight: bold;
-`
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	padding: 10px 0;
+	font-weight: bold;
+`;
 
 const Wrapper = styled.div`
 	display: flex;
@@ -17,37 +19,48 @@ const Wrapper = styled.div`
 `;
 
 const Name = styled.h1`
-  margin: 0;
-  cursor: pointer;
-`
+	margin: 0;
+	cursor: pointer;
+`;
 
 const Menu = styled.div`
-  display: flex;
-  gap: 25px;
-`
+	display: flex;
+	gap: 45px;
+`;
 
 const Item = styled.a`
-  font-size: 1.2rem;
-  cursor: pointer;
+	display: flex;
+	align-items: center;
+	gap: 8px;
+	font-size: 1.2rem;
+	cursor: pointer;
 	transition: transform 0.3s ease-in-out;
 	&:hover {
 		transform: translate(0, -5px);
 	}
 `;
 
+const Icon = styled(DownloadIcon)`
+	height: 1.2rem;
+`;
+
 const Navbar = () => {
-  return (
+	return (
 		<Nav>
 			<Wrapper>
 				<Name>David Lin</Name>
 				<Menu>
 					<Item href='#projects'>Projects</Item>
 					<Item href='#about'>About Me</Item>
+					<Item href={ResumePdf} download='David_Lin_Resume.pdf'>
+						<div>Resume</div>
+						<Icon fill='white'></Icon>
+					</Item>
 					<Item>Contact</Item>
 				</Menu>
 			</Wrapper>
 		</Nav>
 	);
-}
+};
 
-export default Navbar
+export default Navbar;
