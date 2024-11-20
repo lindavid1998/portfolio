c64010e38d44fb79c5eed1e03bad57c721704de7
David Lin <lindavid1998@gmail.com>
2024-11-20 13:18:15 -0800

    Update hero text to provide more detail on background

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,67 +1,75 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import meImg from '../assets/me.jpg';
-import Typed from 'typed.js';
 
 const Container = styled.div`
 	display: flex;
-	align-items: center;
-	justify-content: center;
 	gap: 50px;
 	width: 60%;
+	max-width: 950px;
 	margin: 0 auto;
 
 	@media (max-width: 1400px) {
+		width: 80%;
+		align-items: center;
 		flex-direction: column;
 	}
 `;
 
-const Image = styled.img`
+const Image = styled.div`
 	border-radius: 50%;
 	height: 320px;
+	width: 320px;
 	min-width: 320px;
 	object-fit: cover;
 	transform-origin: center;
+	overflow: hidden;
 `;
 
-const TextWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
+const Text = styled.h4`
+	width: fit-content;
 	position: relative;
-	height: 100px;
-	gap: 30px;
-`;
+	background-color: var(--accent-color);
+	padding: 0.5em 1.2rem;
+	left: -1.2rem;
+	border-radius: 14px;
 
-const Text = styled.h3`
-	width: 500px;
-	font-size: clamp(1.5rem, 3vw, 1.728rem);
 	@media (max-width: 1400px) {
 		text-align: center;
+		left: 0;
 	}
 `;
 
+const Intro = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
+`;
+
 const Hero = () => {
-	const el = useRef(null);
-
-	useEffect(() => {
-		const typed = new Typed(el.current, {
-			strings: [
-				`> Hello! I'm David, and I am seeking a Software Engineering role.
-				Discover my projects and skills below!`,
-			],
-			startDelay: 300,
-			typeSpeed: 20,
-		});
-	}, []);
-
 	return (
-		<Container>
-			<Image src={meImg}></Image>
-			<TextWrapper>
+		<Container id='hero'>
+			<Image>
+				<img src={meImg} style={{ width: '100%', height: '100%' }} />
+			</Image>
+			<Intro>
 				<Text>
-					<span ref={el}></span>
+					Hi! I'm a full stack web developer with React/PostgreSQL/Node.js
+					experience.
 				</Text>
-			</TextWrapper>
+				<p>
+					I come from an operations background with 3+ YoE as a process engineer
+					where I sustained and scaled up high volume medical device
+					manufacturing lines. Through that experience, I’ve developed a strong
+					foundation in operational efficiency, problem-solving under
+					high-stakes conditions, and implementing scalable solutions with
+					strategic thinking.{' '}
+					<strong style={{ color: 'white' }}>
+						Now, I apply those same principles to software engineering—building
+						efficient, reliable, and scalable applications.
+					</strong>
+				</p>
+			</Intro>
 		</Container>
 	);
 };
