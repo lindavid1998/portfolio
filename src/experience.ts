7edeaa05113b2f555a53b46ca457557e91b986c5
lindavid1998 <lindavid1998@gmail.com>
2025-07-22 20:53:59 -0700

    Add experience data store

---
+++ src/experience.ts
@@ -0,0 +1,20 @@
+import { type Skill } from "./types";
+
+interface WorkExperience {
+   company: string;
+   description: string;
+   stack: Skill[];
+}
+
+export const workExperiences: WorkExperience[] = [
+   {
+      company: 'Nummi',
+      description: 'Full stack engineering',
+      stack: ['TypeScript', 'Next.js', 'PostgreSQL']
+   },
+   {
+      company: 'Illumina',
+      description: 'Data scripting',
+      stack: ['Python']
+   }
+]
