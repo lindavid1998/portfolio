d097483ad942945f6b99f932fd6bfefc9e2a4bb3
lindavid1998 <lindavid1998@gmail.com>
2025-07-22 22:22:04 -0700

    Add github stats

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import GitHubStats from "./components/GithubStats";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import WorkExperience from "./components/WorkExperience";
 
 function App() {
   return (
-    <div className="flex flex-col gap-16">
+    <div className="flex flex-col gap-16 p-8">
       <Hero />
       <Skills />
       <WorkExperience />
+      <GitHubStats />
     </div>
   );
 }
---
+++ src/components/GithubStats.tsx
@@ -0,0 +1,11 @@
+import Section from "./Section";
+
+export default function GitHubStats() {
+  return (
+    <Section title="GitHub Stats">
+      <a href="https://github-readme-stats.vercel.app/api?username=lindavid1998&count_private=true&show_icons=true&theme=tokyonight&rank_icon=github&hide=stars&show=reviews">
+        <img src="https://github-readme-stats.vercel.app/api?username=lindavid1998&count_private=true&show_icons=true&theme=tokyonight&rank_icon=github&hide=stars&show=reviews" />
+      </a>
+    </Section>
+  );
+}
