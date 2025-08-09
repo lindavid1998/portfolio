7c8afb8457f0f2a4b4d7de8c759457eee27622b9
lindavid1998 <lindavid1998@gmail.com>
2025-08-08 22:09:50 -0700

    Improve hero text width on small screens

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,70 +1,76 @@
-import React from 'react';
-import styled from 'styled-components';
-import meImg from '../assets/me.jpeg';
+import React from "react";
+import styled from "styled-components";
+import meImg from "../assets/me.jpeg";
 
 const Container = styled.div`
-	display: flex;
-	gap: 50px;
-	width: 60%;
-	max-width: 950px;
-	margin: 0 auto;
-	align-items: center;
+  display: flex;
+  gap: 50px;
+  width: 60%;
+  max-width: 950px;
+  margin: 0 auto;
+  align-items: center;
 
-	@media (max-width: 1400px) {
-		width: 80%;
-		align-items: center;
-		flex-direction: column;
-	}
+  @media (max-width: 1400px) {
+    width: 80%;
+    align-items: center;
+    flex-direction: column;
+  }
 `;
 
 const Image = styled.div`
-	border-radius: 50%;
-	height: 320px;
-	width: 320px;
-	min-width: 320px;
-	object-fit: cover;
-	transform-origin: center;
-	overflow: hidden;
+  border-radius: 50%;
+  height: 320px;
+  width: 320px;
+  min-width: 320px;
+  object-fit: cover;
+  transform-origin: center;
+  overflow: hidden;
 `;
 
 const Text = styled.h4`
-	width: fit-content;
-	position: relative;
-	background-color: var(--accent-color);
-	padding: 0.5em 1.2rem;
-	left: -1.2rem;
-	border-radius: 14px;
+  width: fit-content;
+  position: relative;
+  background-color: var(--accent-color);
+  padding: 0.5em 1.2rem;
+  left: -1.2rem;
+  border-radius: 14px;
+  max-width: 700px;
 
-	@media (max-width: 1400px) {
-		text-align: center;
-		left: 0;
-	}
+  @media (max-width: 1400px) {
+    text-align: center;
+    left: 0;
+  }
 `;
 
 const Intro = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
 `;
 
+const Detail = styled.p`
+	max-width: 700px;
+`
+
 const Hero = () => {
-	return (
-		<Container id='hero'>
-			<Image>
-				<img src={meImg} style={{ width: '100%', height: '100%' }} />
-			</Image>
-			<Intro>
-				<Text className='glow-gradient'>
-					Hi! I'm a full stack web developer with TypeScript/React/PostgreSQL/Node.js
-					experience.
-				</Text>
-				<p>
-					I'm passionate about building software with high impact.
-					Interests include: health tech, sports, travel, green energy, and productivity.
-				</p>
-			</Intro>
-		</Container>
-	);
+  return (
+    <Container id="hero">
+      <Image>
+        <img src={meImg} style={{ width: "100%", height: "100%" }} />
+      </Image>
+      <Intro>
+        <Text className="glow-gradient">
+          Hi! I'm a full stack web developer with TypeScript, React, PostgreSQL,
+          and Node.js experience.
+        </Text>
+        <Detail>
+          I'm passionate about building software with high impact. Interests
+          include: health tech, sports, travel, green energy, and productivity.
+        </Detail>
+      </Intro>
+    </Container>
+  );
 };
 
 export default Hero;
